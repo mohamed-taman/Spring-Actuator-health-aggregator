b64074e6556e42cdbed7f3556b2827c0566228a3
Mohamed Taman <mohamed.taman@gmail.com>
2020-04-22 16:02:19 +0200

    reorganize imports

--- src/main/java/com/siriusx/composite/health/CompositeHealthApplication.java
+++ src/main/java/com/siriusx/composite/health/CompositeHealthApplication.java
@@ -3,7 +3,6 @@ package com.siriusx.composite.health;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.health.CompositeReactiveHealthContributor;
 import org.springframework.boot.actuate.health.CompositeReactiveHealthIndicator;
